1603a0f68cc0dcbb09f7026cc53587450ad57428
Arpit Jha <arpitjha867@gmail.com>
2022-09-17 16:28:47 +0530

    added regex to split the words and count them so that new line is not considered as a new word

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -63,7 +63,7 @@ export default function TextForm(props) {
     </div>
     <div className={`container my-3 text-${(props.mode)==='light'? 'dark':'light'}`}>
         <h1>Your text summary</h1>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008*(text.split(" ").filter((element)=>{return element.length!==0}).length)} Minutes to read</p>
         <h2>Preview</h2>
         <p>{text}</p>
